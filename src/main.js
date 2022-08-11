1eac93d0d1ac5bdc2831e5e2bb6fa41f590316d0
Elvmx47 <elvmx47@outlook.com>
2022-08-11 18:41:03 +0800

    banners

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const jsonServer = require("json-server");
+const path = require("path");
+const router = jsonServer.router(path.resolve(__dirname, "../db.json"), {});
+const middlewares = jsonServer.defaults({});
+
+const server = express();
+
+router.render = (req, res) => {
+  res.jsonp({
+    code: 0,
+    msg: "ok",
+    data: res.locals.data,
+  });
+};
+
+// 使用中间件
+server.use(middlewares);
+server.use(router);
+
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
